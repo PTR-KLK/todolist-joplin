bcb30b7bbc5cbc9ed1e388f53e779c3b8c97aece
Piotr Kielak <p.kielak@outlook.com>
2020-05-29 13:47:25 +0200

    refactor filter code

--- src/modules/filterTools.js
+++ src/modules/filterTools.js
@@ -1,29 +1,38 @@
-export const filterFolders = (arr) => {
+export const filterFolders = ([todoArr, folderArr]) => {
   const parentIdArr = Array.from(
-    new Set(arr[0].filter((e) => e.is_todo === 1).map((e) => e.parent_id))
+    new Set(todoArr.filter((e) => e.is_todo === 1).map((e) => e.parent_id))
   );
 
-  const filteredArr = [];
+// filter array when its elements have id in parentIdArray or one of element child or nth-child has id in parentIdArray
 
-  arr[1].forEach((e) => {
-    if (parentIdArr.includes(e.id) && !e.children) {
-      filteredArr.push({ id: e.id, title: e.title });
-    } else if (
-      e.children &&
-      e.children.filter((el) => parentIdArr.includes(el.id)).length > 0
-    ) {
-      filteredArr.push({ id: e.id, title: e.title, children: [] });
-      e.children.forEach((el) => {
-        if (parentIdArr.includes(el.id)) {
-          filteredArr[filteredArr.length - 1].children.push({
-            id: el.id,
-            title: el.title,
-            todos: arr[0].filter((ele) => ele.parent_id === el.id),
-          });
-        }
-      });
-    }
-  });
+  const filterArr = (arr) =>
+    arr.filter((e) =>
+      parentIdArr.includes(e.id)
+        ? true
+        : e.children &&
+          e.children.reduce(
+            (total, curr) =>
+              parentIdArr.includes(curr.id) || total
+                ? true
+                : curr.children && filterArr(curr.children),
+            false
+          )
+        ? e
+        : false
+    );
 
-  return filteredArr;
+// apply filter to all elements and its childs and nth-childs and add todos array with id matching element id
+
+  const filterFolders = (arr) => {
+    return filterArr(arr).map((e) => e.children ? (parentIdArr.includes(e.id) ? ({
+      ...e,
+      children: filterFolders(e.children),
+      todos: todoArr.filter((el) => el.parent_id === e.id),
+    }) : ({
+      ...e,
+      children: filterFolders(e.children),
+    }) ): {...e, todos: todoArr.filter((el) => el.parent_id === e.id)});
+  };
+
+  return filterFolders(folderArr);
 };
